81f04255587d3de8cc3195e3dae7ce605beae30b
@a.shajdullin <a.shajdullin@rko-group.ru>
2026-05-21 11:21:34 +0500

    feat: loader ui component

---
+++ app/frontend/src/shared/components/ui/loader/loader.props.ts
@@ -0,0 +1,4 @@
+import type {ComponentProps} from 'react'
+
+export interface LoaderProps extends ComponentProps<'div'> {
+}
---
+++ app/frontend/src/shared/components/ui/loader/loader.tsx
@@ -0,0 +1,12 @@
+import cn from 'clsx'
+import {LoaderCircle} from 'lucide-react'
+import styles from './loader.module.scss'
+import type {LoaderProps} from './loader.props'
+
+export function Loader({className, ...rest}: LoaderProps) {
+    return (
+        <div role="status" className={cn(styles.loader, className)} aria-label="Загрузка" {...rest}>
+            <LoaderCircle className={styles.loader__icon}/>
+        </div>
+    )
+}
